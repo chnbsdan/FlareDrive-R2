40119e76b355fbb3fe71a6de7d60f814ed728559
chnbsdan <chnbsdan@gmail.com>
2025-08-15 11:29:50 +0800

    Update Footer.vue

--- assets/Footer.vue
+++ assets/Footer.vue
@@ -20,11 +20,11 @@
     </div>
 
     <div class="project-links">
-      <strong>项目:</strong><a href="https://github.com/willow-god/FlareDrive-R2" target="_blank"
+      <strong>BSDAN:</strong><a href="https://newsnow.hangdn.com/" target="_blank"
         rel="noopener noreferrer">FlareDrive-R2</a>
-      <strong>作者:</strong><a href="https://github.com/willow-god" target="_blank"
+      <strong>采购批发:</strong><a href="https:/szhddz.1688.com" target="_blank"
         rel="noopener noreferrer">LiuShen</a>
-      <strong>初始项目:</strong><a href="https://github.com/ljxi/Cloudflare-R2-oss" target="_blank"
+      <strong>智能家居制造商:</strong><a href="https://github.com/ljxi/Cloudflare-R2-oss" target="_blank"
         rel="noopener noreferrer">Cloudflare-R2-OSS</a>
     </div>
 
@@ -40,10 +40,10 @@ export default {
   name: "Footer",
   data() {
     return {
-      homeUrl: "https://www.liushen.fun/",
-      blogUrl: "https://blog.liushen.fun/",
-      githubUrl: "https://github.com/willow-god",
-      emailUrl: "mailto:01@liushen.fun"
+      homeUrl: "https://www.hangdn.com/",
+      blogUrl: "https://bsdan.nyc.mn/",
+      githubUrl: "https://github.com/chnbsdan",
+      emailUrl: "mailto:sfx@hangdn.com"
     };
   }
 };
